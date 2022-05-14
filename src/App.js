054fc8442df18949c55d5861fd2129e6a899d70a
Thelalitagarwal <lalitagarwal8278@gmail.com>
2022-05-14 21:08:10 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+//c267adf75aaf4e5fa96550c09110d8a1
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Navbar from './components/Navbar';
+import News from './components/News';
+import React, { Component } from 'react'
+import { render } from "react-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar/>
+            <Routes>
+              <Route exact path="/" element={<News key="General" pageSize={6} country="in" cateogry="general"/>}/>
+              <Route exact path="/business" element={<News key="Business" pageSize={6} country="in" cateogry="business"/>}/>
+              <Route exact path="/entertainment"element={<News key="Entertainment" pageSize={6} country="in" cateogry="entertainment"/>}/>
+              <Route exact path="/general"element={<News key="General" pageSize={6} country="in" cateogry="general"/>}/>
+              <Route exact path="/health"element={<News key="Health" pageSize={6} country="in" cateogry="health"/>}/>
+              <Route exact path="/science"element={<News key="Science" pageSize={6} country="in" cateogry="science"/>}/>
+              <Route exact path="/sports"element={<News key="Sports" pageSize={6} country="in" cateogry="sports"/>}/>
+              <Route exact path="/technology"element={<News key="Technology" pageSize={6} country="in" cateogry="technology"/>}/>
+          </Routes>
+        </Router>
+      </div>
+      
+    )
+  }
 }
 
-export default App;
